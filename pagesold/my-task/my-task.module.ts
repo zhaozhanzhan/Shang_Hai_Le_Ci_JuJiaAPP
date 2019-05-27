00903c9e6728109dfca8bebbe5c6033e6a42abef
zhaozhanzhan <799316652@qq.com>
2019-05-27 10:53:24 +0800

    删除文件，修改配置

--- pagesold/my-task/my-task.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { IonicPageModule, IonicModule } from "ionic-angular";
-import { MyTaskPage } from "./my-task";
-@NgModule({
-  declarations: [MyTaskPage],
-  imports: [IonicPageModule.forChild(MyTaskPage), IonicModule],
-  exports: [MyTaskPage]
-})
-export class MyTaskPageModule {}
